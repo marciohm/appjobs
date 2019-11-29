251392f6bab67f2b393514f82ac13efb6fede930
Marcio Rosa <marcio.rosa@rsxtecnologia.com.br>
2019-11-28 21:57:04 -0300

    First attempt

--- index.php
+++ index.php
@@ -5,50 +5,49 @@
  *
  * @return Companies List
  */
-function getCompanies()
-{
-    return [
-        [
-            'name' => 'Company A',
-            'requirements' => 'an apartment or house, and property insurance'
-        ],
-        [
-            'name' => 'Company B',
-            'requirements' => '5 door car or 4 door car, and a driver\'s license and car insurance'
-        ],
-        [
-            'name' => 'Company C',
-            'requirements' => 'social security number and a work permit'
-        ],
-        [
-            'name' => 'Company D',
-            'requirements' => 'an apartment or a flat or a house'
-        ],
-        [
-            'name' => 'Company E',
-            'requirements' => 'a driver\'s license and a 2 door car or a 3 door car or a 4 door car or a 5 door car'
-        ],
-        [
-            'name' => 'Company F',
-            'requirements' => 'a scooter or a bike, or a motorcycle and a driver\'s license and motorcycle insurance'
-        ],
-        [
-            'name' => 'Company G',
-            'requirements' => 'a massage qualification certificate and a liability insurance'
-        ],
-        [
-            'name' => 'Company H',
-            'requirements' => 'a storage place or a garage'
-        ],
-        [
-            'name' => 'Company J',
-            'requirements' => ''
-        ],
-        [
-            'name' => 'Company K',
-            'requirements' => 'a PayPal account'
-        ]
-    ];
+function getCompanies() {
+	return [
+		[
+			'name'         => 'Company A',
+			'requirements' => 'an apartment or house, and property insurance'
+		],
+		[
+			'name'         => 'Company B',
+			'requirements' => '5 door car or 4 door car, and a driver\'s license and car insurance'
+		],
+		[
+			'name'         => 'Company C',
+			'requirements' => 'social security number and a work permit'
+		],
+		[
+			'name'         => 'Company D',
+			'requirements' => 'an apartment or a flat or a house'
+		],
+		[
+			'name'         => 'Company E',
+			'requirements' => 'a driver\'s license and a 2 door car or a 3 door car or a 4 door car or a 5 door car'
+		],
+		[
+			'name'         => 'Company F',
+			'requirements' => 'a scooter or a bike, or a motorcycle and a driver\'s license and motorcycle insurance'
+		],
+		[
+			'name'         => 'Company G',
+			'requirements' => 'a massage qualification certificate and a liability insurance'
+		],
+		[
+			'name'         => 'Company H',
+			'requirements' => 'a storage place or a garage'
+		],
+		[
+			'name'         => 'Company J',
+			'requirements' => ''
+		],
+		[
+			'name'         => 'Company K',
+			'requirements' => 'a PayPal account'
+		]
+	];
 }
 
 /**
@@ -57,42 +56,41 @@ function getCompanies()
  * @param Array $userAttributes
  * @return Array $Companies
  */
-function getJobs($userAttributes)
-{
-    //Iniciate Returns
-    $goodMatches = [];
-    $badMatches = [];
+function getJobs($userAttributes) {
+	//Iniciate Returns
+	$goodMatches = [];
+	$badMatches  = [];
 
-    //Get Companies List
-    $companies = getCompanies();
+	//Get Companies List
+	$companies = getCompanies();
 
-    //Run each company
-    for ($i = 0; $i < count($companies); $i++) {
+	//Run each company
+	for ($i = 0; $i < count($companies); $i++) {
 
-        //Run user attributes
-        foreach ($userAttributes as $attribute) {
+		//Run user attributes
+		foreach ($userAttributes as $attribute) {
 
-            //Set Company Ranking by counting the number of occurrences of an attribute
-            if (isset($companies[$i]['ranking'])) {
-                $companies[$i]['ranking'] += substr_count($companies[$i]['requirements'], $attribute);
-            } else {
-                $companies[$i]['ranking'] = substr_count($companies[$i]['requirements'], $attribute);
-            }
-        }
+			//Set Company Ranking by counting the number of occurrences of an attribute
+			if (isset($companies[$i]['ranking'])) {
+				$companies[$i]['ranking'] += substr_count($companies[$i]['requirements'], $attribute);
+			} else {
+				$companies[$i]['ranking'] = substr_count($companies[$i]['requirements'], $attribute);
+			}
+		}
 
-        //Splits good matches and bad matches
-        if ($companies[$i]['ranking'] > 0) {
-            $goodMatches[] = $companies[$i];
-        } else {
-            $badMatches[] = $companies[$i];
-        }
-    }
+		//Splits good matches and bad matches
+		if ($companies[$i]['ranking'] > 0) {
+			$goodMatches[] = $companies[$i];
+		} else {
+			$badMatches[] = $companies[$i];
+		}
+	}
 
-    //Order good matches list by ranking
-    usort($goodMatches, "method1");
+	//Sort good matches list by ranking
+	usort($goodMatches, "method1");
 
-    //Return list
-    return ['good_matches' => $goodMatches, 'bad_matches' => $badMatches];
+	//Return list
+	return ['good_matches' => $goodMatches, 'bad_matches' => $badMatches];
 }
 
 /**
@@ -101,9 +99,8 @@ function getJobs($userAttributes)
  * @param Array $a, $b
  * @return Array ordered Array
  */
-function method1($a, $b)
-{
-    return ($a['ranking'] >= $b['ranking']) ? -1 : 1;
+function method1($a, $b) {
+	return ($a['ranking'] >= $b['ranking'])?-1:1;
 }
 
 echo "<pre>";
